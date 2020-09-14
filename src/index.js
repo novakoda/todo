afa2cfef72359e5ae9ce54cdeea10e1fcd9e245c
JR Nova <novakoda9@gmail.com>
2020-09-14 01:33:27 -0500

    added basic logic for displaying items

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import _ from 'lodash';
 import {todoItem} from './todoItem.js'
 
-function component() {
-  const element = document.createElement('div');
-  let item = todoItem({
-    "title": "do dis plz",
+function list() {
+  let item1 = todoItem({
+    "title": "I'm 1st",
     "description": "it need to be done asap",
+    "priority": "1"
+  });
+
+  let item2 = todoItem({
+    "title": "I'm 2nd",
+    "description": "it need to be done soon",
+    "priority": "2"
+  });
+
+  let item3 = todoItem({
+    "title": "I'm 3rd",
+    "description": "it just needs to be done",
     "priority": "3"
   });
 
-  element.innerHTML = `
-    <h4 class="item-title">${item.getTitle()}</h4>
-    <p class="item-priority">${item.getPriority()}</p>
-    <p class="item-description">${item.getDescription()}</p>
-  `;
+  let items = [item1, item2, item3];
 
-  return element;
-}
+  items.forEach((item, i) => {
+    const itemDiv = document.createElement('div');
+    itemDiv.innerHTML = `
+      <h4 class="item-title">${item.getTitle()}</h4>
+      <p class="item-priority">${item.getPriority()}</p>
+      <p class="item-description">${item.getDescription()}</p>
+    `;
+    document.body.appendChild(itemDiv);
+  });
+};
 
-
-
-
-document.body.appendChild(component());
+list();
