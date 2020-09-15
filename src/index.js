87347ab8cb0b252ef21fc2b920875d413b9aabc2
JR Nova <novakoda9@gmail.com>
2020-09-14 20:41:53 -0500

    added basic form for adding tasks

--- src/index.js
+++ src/index.js
@@ -1,36 +1,63 @@
 import _ from 'lodash';
 import {todoItem} from './todoItem.js'
 
+let taskItems = [];
+
+let item1 = todoItem({
+  "title": "I'm 1st",
+  "description": "it need to be done asap",
+  "priority": "1"
+});
+
+let item2 = todoItem({
+  "title": "I'm 2nd",
+  "description": "it need to be done soon",
+  "priority": "2"
+});
+
+let item3 = todoItem({
+  "title": "I'm 3rd",
+  "description": "it just needs to be done",
+  "priority": "3"
+});
+
+taskItems.push(item1,item2,item3)
+
+function addTask() {
+  let title = document.getElementById('addTaskName').value;
+  let description = document.getElementById('addTaskDescription').value;
+  let priority = document.getElementById('addTaskPriority').value;
+
+  let task = todoItem({
+    "title": title,
+    "description": description,
+    "priority": priority
+  });
+
+  taskItems.push(task);
+  list();
+};
+
 function list() {
-  let item1 = todoItem({
-    "title": "I'm 1st",
-    "description": "it need to be done asap",
-    "priority": "1"
-  });
+  const tasksDiv = document.getElementById('tasks');
 
-  let item2 = todoItem({
-    "title": "I'm 2nd",
-    "description": "it need to be done soon",
-    "priority": "2"
-  });
-
-  let item3 = todoItem({
-    "title": "I'm 3rd",
-    "description": "it just needs to be done",
-    "priority": "3"
-  });
-
-  let items = [item1, item2, item3];
-
-  items.forEach((item, i) => {
+  taskItems.forEach((item, i) => {
     const itemDiv = document.createElement('div');
     itemDiv.innerHTML = `
       <h4 class="item-title">${item.getTitle()}</h4>
       <p class="item-priority">${item.getPriority()}</p>
       <p class="item-description">${item.getDescription()}</p>
     `;
-    document.body.appendChild(itemDiv);
+    tasksDiv.appendChild(itemDiv);
   });
 };
 
 list();
+
+document.getElementById('newTaskBtn').addEventListener('click', function() {
+    // show new task menu
+});
+
+document.getElementById('addTaskBtn').addEventListener('click', function() {
+  addTask(this);
+});
